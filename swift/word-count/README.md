7a41ed06988a595d16ca2917b88792b365f68836
Damien Burke <damien@damienburke.com>
2015-06-30 11:11:30 -0700

    Add word count exercise

---
+++ swift/word-count/README.md
@@ -0,0 +1,24 @@
+# word count
+
+write a program that given a phrase can count the occurrences of each word in that phrase.
+
+for example for the input `"olly olly in come free"`
+
+```plain
+olly: 2
+in: 1
+come: 1
+free: 1
+```
+
+
+## setup
+
+go through the project setup instructions for xcode using swift:
+
+http://help.exercism.io/getting-started-with-swift.html
+
+
+## source
+
+the golang tour [view source](http://tour.golang.org)
---
+++ swift/word-count/WordCount.swift
@@ -0,0 +1,39 @@
+//
+//  WordCount.swift
+//  Word Count
+//
+//  Created by Damien Burke on 6/1/15.
+//  Copyright (c) 2015 Damien Burke. All rights reserved.
+//
+
+import Foundation
+
+class WordCount {
+  let inputString:String
+  
+  init(words:String){
+    inputString = words
+  }
+  
+  func count() -> Dictionary<String,Int> {
+    var runningCount = [String:Int]()
+    var wordList = getWordList(inputString)
+  
+    for word in wordList {
+      if let previousCount = runningCount[word] {
+        runningCount[word] = previousCount + 1
+      } else {
+        runningCount[word] = 1
+      }
+    }
+    return runningCount
+  }
+    
+  private func getWordList(input:String) -> Array<String> {
+    let lowercased_input = input.lowercaseString
+    let unwantedChars = NSCharacterSet.alphanumericCharacterSet().invertedSet
+    var words = lowercased_input.componentsSeparatedByCharactersInSet(unwantedChars)
+    words = words.filter({$0 != ""})
+    return words
+  }
+}
