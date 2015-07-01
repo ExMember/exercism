a99ed9ff4c3a824f58f096f9733c4b8351021943
Damien Burke <damien@damienburke.com>
2015-07-01 12:47:52 -0700

    Use global sorted() to simplify isAnagram()

--- swift/anagram/Anagram.swift
+++ swift/anagram/Anagram.swift
@@ -1,10 +1,10 @@
 class Anagram {
   let word:String
-  let charCount:[Character:Int]
+  let sorted_chars:[Character]
   
   init(word: String){
     self.word = word
-    self.charCount = Anagram.createCharCount(word)
+    self.sorted_chars = sorted(word.lowercaseString)
   }
   
   func match(candidates:[String]) -> [String]{
@@ -13,20 +13,6 @@ class Anagram {
   
   private func isAnagram(candidate:String) -> Bool {
     if candidate.lowercaseString == word.lowercaseString { return false }
-    let candidateCharCount = Anagram.createCharCount(candidate)
-    return candidateCharCount == self.charCount
-  }
-  
-  private class func createCharCount(word:String) -> [Character:Int] {
-    var charCount = [Character:Int]()
-    
-    for char in word.lowercaseString {
-      if let count = charCount[char] {
-        charCount[char] = count + 1
-      } else {
-        charCount[char] = 1
-      }
-    }
-    return charCount
+    return sorted_chars == sorted(candidate.lowercaseString)
   }
 }
