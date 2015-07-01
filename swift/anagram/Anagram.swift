211bce82334bf36ff41015861293384b089aa44c
Damien Burke <damien@damienburke.com>
2015-06-30 21:36:37 -0700

    Add anagram finding

--- swift/anagram/Anagram.swift
+++ swift/anagram/Anagram.swift
@@ -1,8 +1,30 @@
 class Anagram {
+  let word:String
+  let charCount:[Character:Int]
+  
   init(word: String){
+    self.word = word
+    self.charCount = Anagram.createCharCount(word)
   }
   
-  func match([String]) -> [String]{
-    return []
+  func match(candidates:[String]) -> [String]{
+    var anagrams = candidates.filter{ (candidate) -> Bool in
+      let candidateCharCount = Anagram.createCharCount(candidate)
+      return candidateCharCount == self.charCount
+    }
+    return anagrams
+  }
+  
+  private class func createCharCount(word:String) -> [Character:Int] {
+    var charCount = [Character:Int]()
+    
+    for char in word {
+      if let count = charCount[char] {
+        charCount[char] = count + 1
+      } else {
+        charCount[char] = 1
+      }
+    }
+    return charCount
   }
 }
