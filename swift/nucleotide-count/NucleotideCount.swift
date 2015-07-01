7fe7c76bf19c572e42d6b5936956b555eac383f9
Damien Burke <damien@damienburke.com>
2015-07-01 13:07:18 -0700

    Make DNA class pass all specs

--- swift/nucleotide-count/NucleotideCount.swift
+++ swift/nucleotide-count/NucleotideCount.swift
@@ -1,12 +1,26 @@
 class DNA {
+  var bases:[String:Int]
+  
   init?(strand:String){
+    bases = [ "T": 0, "A": 0, "C": 0, "G": 0]
+    for base in strand {
+      if let count = bases[String(base)] {
+        bases[String(base)] = count + 1
+      } else {
+        return nil
+      }
+    }
   }
   
   func count(base:String) -> Int {
-    return 0
+    if let count = bases[base] {
+      return count
+    } else {
+      return 0
+    }
   }
   
   func counts() -> [String:Int] {
-    return [:]
+    return bases
   }
 }
