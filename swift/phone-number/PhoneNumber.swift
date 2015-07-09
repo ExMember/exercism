97f0f052662bee1ee7ec810dcffce29b92d04f8e
Damien Burke <damien@damienburke.com>
2015-07-09 13:58:50 -0700

    Clean number

--- swift/phone-number/PhoneNumber.swift
+++ swift/phone-number/PhoneNumber.swift
@@ -1,9 +1,23 @@
 class PhoneNumber {
+  let normalizedNumber:String
+
   init(startingNumber:String){
+    normalizedNumber = PhoneNumber.filterNonDigits(startingNumber)
+  }
+
+  private class func filterNonDigits(string:String) -> String {
+    var digits:Set<Character> = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    var filteredString = ""
+    for character in string {
+      if digits.contains(character) {
+        filteredString.append(character)
+      }
+    }
+    return filteredString
   }
 
   func number() -> String {
-    return ""
+    return normalizedNumber
   }
 
   func areaCode() -> String {
