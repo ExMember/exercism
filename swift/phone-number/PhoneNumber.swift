2fd8035382d4dabd004c0ff98a853b613ffbad15
Damien Burke <damien@damienburke.com>
2015-07-09 13:39:41 -0700

    Get phone number specs to compile

---
+++ swift/phone-number/PhoneNumber.swift
@@ -0,0 +1,16 @@
+class PhoneNumber {
+  init(startingNumber:String){
+  }
+
+  func number() -> String {
+    return ""
+  }
+
+  func areaCode() -> String {
+    return ""
+  }
+
+  func description() -> String {
+    return ""
+  }
+}
