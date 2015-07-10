fa8b9be9d54ed3e27d21a76f3b1b4c606be29556
Damien Burke <damien@damienburke.com>
2015-07-10 14:57:52 -0400

    Get grade school to pass

---
+++ swift/grade-school/GradeSchool.swift
@@ -0,0 +1,15 @@
+class GradeSchool {
+  var db:[Int:[String]] = [:]
+
+  func addStudent(name:String, grade:Int) -> [Int: [String]]{
+    return [:]
+  }
+
+  func studentsInGrade(grade:Int) -> [String]{
+    return []
+  }
+
+  func sortedRoster() -> [Int:[String]]{
+    return [:]
+  }
+}
