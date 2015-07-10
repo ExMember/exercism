e06d732cb8c96eabccab2bcd0128911b3aca3803
Damien Burke <damien@damienburke.com>
2015-07-10 15:16:03 -0400

    Pass the grade school tests

--- swift/grade-school/GradeSchool.swift
+++ swift/grade-school/GradeSchool.swift
@@ -2,14 +2,21 @@ class GradeSchool {
   var db:[Int:[String]] = [:]
 
   func addStudent(name:String, grade:Int) -> [Int: [String]]{
-    return [:]
+    if db[grade] == nil {
+      db[grade] = []
+    }
+    db[grade]!.append(name)
+    return db
   }
 
   func studentsInGrade(grade:Int) -> [String]{
-    return []
+    return db[grade] ?? []
   }
 
   func sortedRoster() -> [Int:[String]]{
-    return [:]
+    for (grade, var students) in db {
+      db[grade] = sorted(students, <)
+    }
+    return db
   }
 }
