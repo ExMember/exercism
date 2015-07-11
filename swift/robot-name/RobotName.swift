dd6ff911e506040512baac0f8a240c16d2f25ca3
Damien Burke <damien@damienburke.com>
2015-07-10 21:36:35 -0400

    Pass the robot name tests

--- swift/robot-name/RobotName.swift
+++ swift/robot-name/RobotName.swift
@@ -1,5 +1,17 @@
 class Robot {
-  let name = ""
-  func resetName(){
+  var name:String
+
+  init() {
+    name = Robot.generateName()
+  }
+
+  func resetName() {
+    self.name = Robot.generateName()
+  }
+
+  private static var number = 1
+  class private func generateName() -> String {
+    number = (number + 1) % 1000
+    return "AA" + String(format: "%03d", number)
   }
 }
