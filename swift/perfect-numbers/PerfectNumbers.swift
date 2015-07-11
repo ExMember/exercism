2829f83c1a938d01a477d1eca0649f04254a4fb5
Damien Burke <damien@damienburke.com>
2015-07-11 10:11:37 -0400

    Get perfect numbers tests to pass

--- swift/perfect-numbers/PerfectNumbers.swift
+++ swift/perfect-numbers/PerfectNumbers.swift
@@ -1,6 +1,28 @@
 class NumberClassifier {
-  let classification:NumberClassification = .Perfect
+  let classification:NumberClassification
+
   init(number:Int) {
+    var factors = NumberClassifier.factorsOf(number)
+    factors.remove(number)
+    var sum = Array(factors).reduce(0, combine:+)
+
+    if sum < number {
+      classification = .Deficient
+    } else if sum > number {
+      classification = .Abundant
+    } else {
+      classification = .Perfect
+    }
+  }
+
+  private class func factorsOf(number:Int) -> Set<Int> {
+    var factors = Set<Int>()
+    for possibleFactor in 1...number {
+      if (number % possibleFactor) == 0 {
+        factors.insert(possibleFactor)
+      }
+    }
+    return factors
   }
 }
 
