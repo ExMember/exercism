d70c7f40382756e5ebde544168c8d6edc6515f26
Damien Burke <damien@damienburke.com>
2015-07-11 00:45:34 -0400

    Refactor

--- swift/robot-name/RobotName.swift
+++ swift/robot-name/RobotName.swift
@@ -9,40 +9,34 @@ class Robot {
     self.name = Robot.generateName()
   }
 
-  private static let LETTERS = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
   private class func generateName() -> String {
     incrementName()
-    var nameFormat = "\(LETTERS[first_letter_index])\(LETTERS[second_letter_index])%03d"
-    return String(format: nameFormat, number)
+    return letters() + digits()
   }
 
-  private class func first_letter() -> String {
-    return String(LETTERS[first_letter_index])
-  }
-
-  private class func second_letter() -> String {
-    return String(LETTERS[second_letter_index])
-  }
-
-  private class func numberPortion() -> String {
-    return ""
-  }
-
+  private static let LETTERS = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
   private static var number = 0
   private static var second_letter_index = 0
   private static var first_letter_index = 0
 
+  private class func letters() -> String {
+    return String([LETTERS[first_letter_index], LETTERS[second_letter_index]])
+  }
+
+  private class func digits() -> String {
+    return String(format:"%03d", number)
+  }
+
   private class func incrementName() {
-    number = number + 1
+    number += 1
     if number >= 1000 {
       number = 0
-      second_letter_index = second_letter_index + 1
+      second_letter_index += 1
     }
 
     if second_letter_index >= LETTERS.count {
       second_letter_index = 0
-      first_letter_index = first_letter_index + 1
+      first_letter_index += 1
     }
 
     if first_letter_index >= LETTERS.count {
