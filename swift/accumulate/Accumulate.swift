33cd348d96ec47ca4c922caabbb7abb604d032e8
Damien Burke <damien@damienburke.com>
2015-07-27 16:59:21 -0700

    Force cast Array elements to be the correct type.

--- swift/accumulate/Accumulate.swift
+++ swift/accumulate/Accumulate.swift
@@ -1,10 +1,9 @@
 extension Array {
   func accumulate<OldType,NewType>(function: (OldType) -> NewType) -> Array<NewType>{
     var newArray = Array<NewType>()
-    var oldArray = Array<OldType>()
 
-    for element in oldArray {
-      newArray.append(function(element))
+    for element in self {
+      newArray.append(function(element as! OldType))
     }
     return newArray
   }
