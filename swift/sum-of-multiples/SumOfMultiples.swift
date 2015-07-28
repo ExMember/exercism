2236acd4ae6c859a7b4a52454ee5ef9f4503f510
Damien Burke <damien@damienburke.com>
2015-07-27 22:45:00 -0700

    Instead of testing all the integers, generate the ones we want

--- swift/sum-of-multiples/SumOfMultiples.swift
+++ swift/sum-of-multiples/SumOfMultiples.swift
@@ -1,25 +1,17 @@
 class SumOfMultiples {
   class func toLimit(limit:Int, inMultiples:[Int] = [3,5]) -> Int{
     var multiples = Set<Int>()
+    var factors = inMultiples.filter {$0 != 0}
     
-    for i in 1..<limit {
-      for multiple in inMultiples {
-        if isDivisibleBy(i, divisor: multiple) {
-          multiples.insert(i)
+      for multiple in factors {
+        for var testNumber = multiple; testNumber < limit; testNumber += multiple {
+          multiples.insert(testNumber)
         }
       }
-    }
     
     return sumSet(multiples)
   }
 
-  private class func isDivisibleBy(number:Int, divisor:Int) -> Bool {
-    if divisor == 0 {
-      return false
-    }
-    return (number % divisor) == 0
-  }
-  
   private class func sumSet(set:Set<Int>) -> Int {
     return Array(set).reduce(0, combine: +)
   }
