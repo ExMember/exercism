268315ee8a3869a54332b0792ccb424de0b033ca
Damien Burke <damien@damienburke.com>
2015-07-27 21:38:00 -0700

    implement toLimit for most cases

--- swift/sum-of-multiples/SumOfMultiples.swift
+++ swift/sum-of-multiples/SumOfMultiples.swift
@@ -1,5 +1,14 @@
 class SumOfMultiples {
   class func toLimit(limit:Int, inMultiples:[Int] = [3,5]) -> Int{
-    return 0
+    var multiples = Set<Int>()
+    
+    for i in 1..<limit {
+      for multiple in inMultiples {
+        if (i % multiple) == 0 {
+          multiples.insert(i)
+        }
+      }
+    }
+    return Array(multiples).reduce(0,combine:+)
   }
 }
