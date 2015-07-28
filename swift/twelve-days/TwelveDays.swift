af3668c56a63699a54bce04233fb23e110795f2d
Damien Burke <damien@damienburke.com>
2015-07-28 11:46:34 -0700

    Get the preamble correct

--- swift/twelve-days/TwelveDays.swift
+++ swift/twelve-days/TwelveDays.swift
@@ -1,6 +1,6 @@
 class TwelveDaysSong {
   class func verse(verseNumber:Int) -> String {
-    return "verse \(verseNumber)"
+    return "\(preamble(verseNumber)) \(giftList(verseNumber)).\n"
   }
   
   class func verses(from:Int, _ to:Int) -> String {
@@ -9,5 +9,21 @@ class TwelveDaysSong {
   
   class func sing() -> String {
     return verses(1, 12)
+  }
+  
+  private static let ORDINALS = [1:"first", 2:"second", 3: "third", 4:"fourth",
+    5:"fifth", 6:"sixth", 7:"seventh", 8:"eighth", 9:"ninth", 10:"tenth",
+    11:"eleventh", 12:"twelfth"]
+  
+  private class func preamble(day:Int) -> String {
+    return "On the \(ordinal(day)) day of Christmas my true love gave to me,"
+  }
+  
+  private class func ordinal(number:Int) -> String {
+    return ORDINALS[number] ?? "\(number)-th"
+  }
+  
+  private class func giftList(day:Int) -> String {
+    return "a Partridge in a Pear Tree"
   }
 }
