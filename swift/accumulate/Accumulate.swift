0310a06142d0ffa2174d116156455efef3ab11c8
Damien Burke <damien@damienburke.com>
2015-07-27 17:07:17 -0700

    Correct the generic function definition to avoid the force cast

--- swift/accumulate/Accumulate.swift
+++ swift/accumulate/Accumulate.swift
@@ -1,9 +1,9 @@
 extension Array {
-  func accumulate<OldType,NewType>(function: (OldType) -> NewType) -> Array<NewType>{
+  func accumulate<NewType>(function: (T) -> NewType) -> Array<NewType>{
     var newArray = Array<NewType>()
 
     for element in self {
-      newArray.append(function(element as! OldType))
+      newArray.append(function(element))
     }
     return newArray
   }
