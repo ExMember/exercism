d8d4630a5723220e7a708d52d5e9671351422ba4
Damien Burke <damien@damienburke.com>
2015-07-29 11:12:53 -0700

    Prevent double rounding of ages

--- swift/space-age/SpaceAge.swift
+++ swift/space-age/SpaceAge.swift
@@ -8,37 +8,40 @@ class SpaceAge {
   }
   
   var onEarth:Double {
-    return roundToTwoDecimalPlaces(Double(seconds) / 31557600.0)
+    return roundToTwoDecimalPlaces(earthAge)
   }
   
   var onMercury:Double {
-    return roundToTwoDecimalPlaces(onEarth / 0.2408467)
+    return roundToTwoDecimalPlaces(earthAge / 0.2408467)
   }
   
   var onVenus:Double {
-    return roundToTwoDecimalPlaces(onEarth / 0.61519726 )
+    return roundToTwoDecimalPlaces(earthAge / 0.61519726 )
   }
   
   var onMars:Double {
-    return roundToTwoDecimalPlaces(onEarth / 1.8808158 )
+    return roundToTwoDecimalPlaces(earthAge / 1.8808158 )
   }
   
   var onJupiter:Double {
-    return roundToTwoDecimalPlaces(onEarth / 11.862615 )
+    return roundToTwoDecimalPlaces(earthAge / 11.862615 )
   }
   
   var onSaturn:Double {
-    return roundToTwoDecimalPlaces(onEarth / 29.447498 )
+    return roundToTwoDecimalPlaces(earthAge / 29.447498 )
   }
   
   var onUranus:Double {
-    return roundToTwoDecimalPlaces(onEarth / 84.016846 )
+    return roundToTwoDecimalPlaces(earthAge / 84.016846 )
   }
   
   var onNeptune:Double {
-    return roundToTwoDecimalPlaces(onEarth / 164.79132 )
+    return roundToTwoDecimalPlaces(earthAge / 164.79132 )
   }
   
+  private var earthAge:Double {
+    return Double(seconds) / 31557600.0
+  }
   private func roundToTwoDecimalPlaces(number:Double) -> Double {
     return round(number * 100) / 100
   }
