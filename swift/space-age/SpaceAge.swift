67d431b211d06e237e60726c8446982db7282d63
Damien Burke <damien@damienburke.com>
2015-07-29 11:08:36 -0700

    Add support for all the planets

--- swift/space-age/SpaceAge.swift
+++ swift/space-age/SpaceAge.swift
@@ -1,15 +1,45 @@
+import Foundation
+
 class SpaceAge {
-  let seconds = 0
-  let onEarth = 0.0
-  let onMercury = 0.0
-  let onVenus = 0.0
-  let onMars = 0.0
-  let onJupiter = 0.0
-  let onSaturn = 0.0
-  let onUranus = 0.0
-  let onNeptune = 0.0
+  let seconds:Int
   
-  init(Int) {
-    
+  init(_ secs:Int) {
+   seconds = secs
+  }
+  
+  var onEarth:Double {
+    return roundToTwoDecimalPlaces(Double(seconds) / 31557600.0)
+  }
+  
+  var onMercury:Double {
+    return roundToTwoDecimalPlaces(onEarth / 0.2408467)
+  }
+  
+  var onVenus:Double {
+    return roundToTwoDecimalPlaces(onEarth / 0.61519726 )
+  }
+  
+  var onMars:Double {
+    return roundToTwoDecimalPlaces(onEarth / 1.8808158 )
+  }
+  
+  var onJupiter:Double {
+    return roundToTwoDecimalPlaces(onEarth / 11.862615 )
+  }
+  
+  var onSaturn:Double {
+    return roundToTwoDecimalPlaces(onEarth / 29.447498 )
+  }
+  
+  var onUranus:Double {
+    return roundToTwoDecimalPlaces(onEarth / 84.016846 )
+  }
+  
+  var onNeptune:Double {
+    return roundToTwoDecimalPlaces(onEarth / 164.79132 )
+  }
+  
+  private func roundToTwoDecimalPlaces(number:Double) -> Double {
+    return round(number * 100) / 100
   }
 }
