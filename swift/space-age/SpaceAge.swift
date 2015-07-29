914e61cfe6d606a412777940f3a7f2f610076b6a
Damien Burke <damien@damienburke.com>
2015-07-29 00:34:36 -0700

    Get SpaceAge tests to compile

---
+++ swift/space-age/SpaceAge.swift
@@ -0,0 +1,15 @@
+class SpaceAge {
+  let seconds = 0
+  let onEarth = 0.0
+  let onMercury = 0.0
+  let onVenus = 0.0
+  let onMars = 0.0
+  let onJupiter = 0.0
+  let onSaturn = 0.0
+  let onUranus = 0.0
+  let onNeptune = 0.0
+  
+  init(Int) {
+    
+  }
+}
