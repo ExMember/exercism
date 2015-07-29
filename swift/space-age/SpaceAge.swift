991f9539ac2452416b0322546f59e0f1bd496d78
Damien Burke <damien@damienburke.com>
2015-07-29 11:16:33 -0700

    Improve spacing

--- swift/space-age/SpaceAge.swift
+++ swift/space-age/SpaceAge.swift
@@ -16,32 +16,33 @@ class SpaceAge {
   }
   
   var onVenus:Double {
-    return roundToTwoDecimalPlaces(earthAge / 0.61519726 )
+    return roundToTwoDecimalPlaces(earthAge / 0.61519726)
   }
   
   var onMars:Double {
-    return roundToTwoDecimalPlaces(earthAge / 1.8808158 )
+    return roundToTwoDecimalPlaces(earthAge / 1.8808158)
   }
   
   var onJupiter:Double {
-    return roundToTwoDecimalPlaces(earthAge / 11.862615 )
+    return roundToTwoDecimalPlaces(earthAge / 11.862615)
   }
   
   var onSaturn:Double {
-    return roundToTwoDecimalPlaces(earthAge / 29.447498 )
+    return roundToTwoDecimalPlaces(earthAge / 29.447498)
   }
   
   var onUranus:Double {
-    return roundToTwoDecimalPlaces(earthAge / 84.016846 )
+    return roundToTwoDecimalPlaces(earthAge / 84.016846)
   }
   
   var onNeptune:Double {
-    return roundToTwoDecimalPlaces(earthAge / 164.79132 )
+    return roundToTwoDecimalPlaces(earthAge / 164.79132)
   }
   
   private var earthAge:Double {
     return Double(seconds) / 31557600.0
   }
+  
   private func roundToTwoDecimalPlaces(number:Double) -> Double {
     return round(number * 100) / 100
   }
