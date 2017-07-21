0ad276b3f69657d96b03422fb48da9bb094f8be2
Damien Burke <damien@damienburke.com>
2017-07-20 23:46:26 -0700

    Use set size and list comprehension

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,3 @@
 def is_isogram(word):
-    letters = set()
-    for char in list(word.lower()):
-        if char.isalpha():
-            if char in letters:
-                return False
-            letters.add(char)
-    return True
+    chars = [char for char in list(word.lower()) if char.isalpha()]
+    return len(set(chars)) == len(chars)
