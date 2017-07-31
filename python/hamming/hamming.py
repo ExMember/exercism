58272639a6f2f803a8dd844e43d2c965fc331ff8
Damien Burke <damien@damienburke.com>
2017-07-30 22:48:04 -0700

    Use list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,4 @@
 def distance(a, b):
     if len(a) != len(b):
         raise ValueError
-    dist = 0
-    chars = zip(a, b)
-    for c in chars:
-        if c[0] != c[1]:
-            dist += 1
-    return dist
+    return len([c for c in zip(a, b) if c[0] != c[1]])
